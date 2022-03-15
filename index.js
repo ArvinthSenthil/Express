31eb515697fc39f58e601ff6a3d5bdb4db478f9c
ArvinthSenthil <arvinthraj1999@gmail.com>
2022-03-15 22:05:50 +0530

    updated

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const movies=[{"id":"100","name":"Iron man 2","poster":"https://m.media-amazon.c
 {"id":"104","name":"Interstellar","poster":"https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg","rating":8.6,"summary":"When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.","trailer":"https://www.youtube.com/embed/zSWdZVtXT7E"},
 {"id":"105","name":"Baahubali","poster":"https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg","rating":8,"summary":"In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.","trailer":"https://www.youtube.com/embed/sOEg_YZQsTI"},
 {"id":"106","name":"Ratatouille","poster":"https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=","rating":8,"summary":"Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.","trailer":"https://www.youtube.com/embed/NgsQ8mVkN8w"}];
-const PORT=4000;
-// middle ware-> intercept ->converting body to json
+const PORT=process.env.PORT;
+
 app.use(express.json());
 
 // const MONGODB_URL="mongodb://localhost";  //server where mongoDB connects
